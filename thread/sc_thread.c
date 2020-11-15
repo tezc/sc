f536399dd476640d0161e66a78dd5cce50c77d3f
tezc <tezc.vx@gmail.com>
2020-11-15 04:21:07 +0300

    thread & url

---
+++ thread/sc_thread.c
@@ -0,0 +1,152 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Ozan Tezcan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "sc_thread.h"
+
+void sc_thread_init(struct sc_thread* thread)
+{
+    thread->id = 0;
+}
+
+#if defined(_WIN32) || defined(_WIN64)
+#include <process.h>
+
+unsigned int sc_thread_fn(void* arg)
+{
+    struct sc_thread* thread = arg;
+
+    thread->ret = thread->fn(thread->arg);
+    return 0;
+}
+
+int sc_thread_start(struct sc_thread* thread, void* (*fn)(void*), void* arg)
+{
+    int rc;
+
+    thread->fn = fn;
+    thread->arg = arg;
+
+    thread->id = (HANDLE)_beginthreadex(NULL, 0, sc_thread_fn, thread, 0, NULL);
+    rc = thread->id == 0 ? -1 : 0;
+
+    return rc;
+}
+
+int sc_thread_stop(struct sc_thread* thread, void** ret)
+{
+    int rc = 0;
+    DWORD rv;
+    BOOL brc;
+
+    if (thread->id == 0) {
+        goto out;
+    }
+
+    rv = WaitForSingleObject(thread->id, INFINITE);
+    if (rv == WAIT_FAILED) {
+        rc = -1;
+    }
+
+    brc = CloseHandle(thread->id);
+    if (!brc) {
+        rc = -1;
+        goto out;
+    }
+
+out:
+    thread->id = 0;
+
+    if (ret != NULL) {
+        *ret = thread->ret;
+    }
+
+    return rc;
+}
+#else
+
+int sc_thread_start(struct sc_thread* thread, void* (*fn)(void*), void* arg)
+{
+    int rc;
+    pthread_attr_t hndl;
+
+    rc = pthread_attr_init(&hndl);
+    if (rc != 0) {
+        goto error;
+    }
+
+    rc = pthread_attr_setdetachstate(&hndl, PTHREAD_CREATE_JOINABLE);
+    if (rc != 0) {
+        goto clean_attr;
+    }
+
+    rc = pthread_create(&thread->id, &hndl, fn, arg);
+    if (rc != 0) {
+        goto clean_attr;
+    }
+
+    // This may only fail with EINVAL.
+    pthread_attr_destroy(&hndl);
+
+    return 0;
+
+clean_attr:
+    pthread_attr_destroy(&hndl);
+error:
+    return -1;
+}
+
+int sc_thread_stop(struct sc_thread* thread, void** ret)
+{
+    int rc;
+    void* val;
+
+    if (thread->id == 0) {
+        goto out;
+    }
+
+    rc = pthread_equal(pthread_self(), thread->id);
+    if (rc != 0) {
+        return -1;
+    }
+
+    rc = pthread_join(thread->id, &val);
+    if (rc != 0) {
+        return -1;
+    }
+
+    thread->id = 0;
+out:
+    if (ret != NULL) {
+        *ret = val;
+    }
+
+    return 0;
+}
+
+#endif
+
+int sc_thread_term(struct sc_thread* thread)
+{
+    return sc_thread_stop(thread, NULL);
+}
