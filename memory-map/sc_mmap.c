dbbd69dcb14a518c8b35b8fdf591736ea91ce9b8
tezc <tezc.vx@gmail.com>
2020-11-25 19:01:48 +0300

    mmap

--- memory-map/sc_mmap.c
+++ memory-map/sc_mmap.c
@@ -1,3 +1,103 @@
 #include "sc_mmap.h"
 
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
+int sc_mmap_init(struct sc_mmap *m, const char *name, int file_flags,
+                 int map_flags, bool shared, size_t offset, size_t len)
+{
+    const int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+    int s;
+
+    *m = (struct sc_mmap){0};
+
+    struct stat st;
+    int fd, rc, saved_errno;
+    size_t size;
+    void *p = NULL;
+
+    fd = open(name, file_flags, mode);
+    if (fd == -1) {
+        goto error;
+    }
+
+    rc = stat(name, &st);
+    if (rc != 0) {
+        goto cleanup_fd;
+    }
+
+    size = st.st_size;
+
+    if (len > size) {
+        size_t seek, written;
+        size_t block = st.st_blksize;
+        size_t pos = (size / block) * block + block - 1;
+
+        for (; pos < len + block - 1; pos += block) {
+            if (pos >= len) {
+                pos = len - 1;
+            }
+
+            seek = lseek(fd, pos, SEEK_SET);
+            if (seek == -1) {
+                goto cleanup_fd;
+            }
+
+            written = write(fd, "", 1);
+            if (written != 1) {
+                goto cleanup_fd;
+            }
+        }
+    }
+
+    s = shared ? MAP_SHARED : MAP_PRIVATE;
+    p = mmap(NULL, len, map_flags, s, fd, offset);
+    if (p == MAP_FAILED) {
+        goto cleanup_fd;
+    }
+
+    m->ptr = p;
+    m->len = len;
+
+    close(fd);
+
+    return 0;
+
+cleanup_fd:
+    saved_errno = errno;
+    close(fd);
+error:
+    strcpy(m->err, strerror(saved_errno));
+
+    return -1;
+}
+
+int sc_mmap_msync(struct sc_mmap *m, size_t offset, size_t len)
+{
+    int rc;
+    char *p = (char *) m->ptr + offset;
+
+    rc = msync(p, len, MS_SYNC);
+    if (rc != 0) {
+        strcpy(m->err, strerror(errno));
+    }
+
+    return rc;
+}
+
+int sc_mmap_term(struct sc_mmap *m)
+{
+    int rc;
+
+    rc = munmap(m->ptr, m->len);
+    if (rc != 0) {
+        strcpy(m->err, strerror(errno));
+    }
+
+    return rc;
+}
--- memory-map/sc_mmap.h
+++ memory-map/sc_mmap.h
@@ -2,15 +2,32 @@
 #define SC_MMAP_H
 
 #include <stdlib.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 
-enum sc_mmap_flag
+#define SC_MMAP_FILE_READ      O_RDONLY
+#define SC_MMAP_FILE_READWRITE O_RDWR
+#define SC_MMAP_FILE_CREATE    O_CREAT
+#define SC_MMAP_FILE_TRUNC     O_TRUNC
+
+#define SC_MMAP_MAP_READ  PROT_READ
+#define SC_MMAP_MAP_WRITE PROT_WRITE
+#define SC_MMAP_MAP_EXEC  PROT_EXEC
+
+
+struct sc_mmap
 {
-    SC_MMAP_READ,
-    SC_MMAP_WRITE,
-    SC_MMAP_EXEC
+    void *ptr;
+    size_t len;
+    char err[64];
 };
 
+int sc_mmap_init(struct sc_mmap *m, const char *name, int file_flags, int map_flags,
+                 bool shared, size_t offset, size_t len);
 
+int sc_mmap_msync(struct sc_mmap *m, size_t offset, size_t len);
+int sc_mmap_term(struct sc_mmap *m);
 
 
 #endif
